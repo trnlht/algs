d75e617335705f889a48df710d03cd9178d3e818
trnlht <trnlht@gmail.com>
2020-01-12 13:15:08 +0300

    Program 3.20 modified

--- chapter_3/pr3_20.cpp
+++ chapter_3/pr3_20.cpp
@@ -2,6 +2,7 @@
 
 #include "point.h"
 
+//! Узел, содержащий точку
 struct pnode
 {
 	point p;
@@ -16,8 +17,10 @@ struct pnode
 	}
 };
 
+//! Указатель на узел, содержащий точку (или связный список таких узлов)
 typedef pnode* plink;
 
+//! Двумерный массив связных списков узлов с точками 
 static plink** grid;
 
 static int G, cnt = 0;
@@ -29,14 +32,46 @@ void gridinsert(float x, float y)
 	
 }
 
+/*!
+ * Выделяет память под двумерный массив связных списков узлов с точками
+ */
+plink** malloc2d(int m, int n)
+{
+	plink** t = new plink*[m];
+
+	for(int i = 0; i < m; i++)
+		t[i] = new plink[n];
+
+	return t;
+}
+
 void pr3_20()
 {
+	using namespace std;
+
 	int i;
 	const int N = 1000000;
 
 	const float d = 0.0001;
 
-	float G = 1/d;
+	int G = 1/d;
 
-	grid = malloc2d(G+2, G+2);
+	grid = malloc2d(G+2, G+2); //(G+2) - размерность сетки 
+
+	for(i = 0; i < (G+2); i++)
+		for(int j = 0; j < (G+2); j++)
+			grid[i][j] = 0;
+
+	for(i = 0; i < N; i++)
+		gridinsert(randFloat(), randFloat());
+
+	cout << cnt << " pairs in radius " << d << endl;
+
 }
+
+
+
+
+
+
+
